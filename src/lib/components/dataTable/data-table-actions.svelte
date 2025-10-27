1ad14074130582bab2a10e1a82ee593305af8370
sphfaar <47697470+sphfaar@users.noreply.github.com>
2025-10-27 09:38:50 +0100

    DataTable debloated from shadcn

--- src/lib/components/dataTable/data-table-actions.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	let { detailsUrl }: { detailsUrl: string | undefined } = $props();
-</script>
-
-<a href={detailsUrl} class={detailsUrl === undefined ? 'hidden' : ''} target="_blank">🔗</a>
